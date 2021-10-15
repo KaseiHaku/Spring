2e6102a61cd741d3845345456b345131bdf1d5e0
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2021-10-15 10:32:01 +0800

    Update AroundAspect.java

--- src/main/java/kasei/spring/aop/aspect/AroundAspect.java
+++ src/main/java/kasei/spring/aop/aspect/AroundAspect.java
@@ -16,8 +16,10 @@ public class AroundAspect {
 	/* 环绕通知需要携带 ProceedingJoinPoin 类型的参数
 	 * 环绕通知类似于动态代理的全过程：ProceedingJoinPoin 类型的参数可以决定是否执行目标方法
 	 * 且环绕通知必须有返回值，返回值即为目标方法的返回值
+	 * 
+	 * 如果 @Around(value="val") val 的值不符合 切点表达式，那么 val 所指定的方法必须被 @Pointcut 注解注释
 	 * */
-	@Around("execution(public int kasei.spring.aop.*.*(..))")
+	@Around("execution(public int kasei.spring.aop.*.*(..))")  
 	public Object aroundMethod(ProceedingJoinPoint pjd){
 		Object result = null;
 		String className = pjd.getTarget().getClass().getName();
