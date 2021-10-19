dd708786ecdb59c386ea943e0791e32879289dbe
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2021-10-19 10:32:11 +0800

    Update LoggingAspect.java

--- src/main/java/kasei/spring/aop/aspect/LoggingAspect.java
+++ src/main/java/kasei/spring/aop/aspect/LoggingAspect.java
@@ -19,7 +19,11 @@ import org.springframework.stereotype.Component;
  * */
 @Order(1)//指定切面的优先级，值越小优先级越高
 @Aspect
-@Component
+/** 
+ * @Component 注解不能和 @Aspect 同时使用，Spring 会自动将 @Aspect 放入 IOC 容器，
+ * 再次添加 @Component 会导致放入两次，CGLIB 代理的话，会导致切面切的实例不是想要的实例 
+ * */
+// @Component
 public class LoggingAspect {
 
 	@Before("execution(public int kasei.spring.aop.ObjImp.add(int, int))")
